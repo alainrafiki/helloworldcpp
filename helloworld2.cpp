e411773282247972768454f5b0d0a96f0bdad8c3
Rafiki Technology <mydeveloper1@users.noreply.github.com>
2019-10-29 15:13:43 -0500

    Initial commit

---
+++ helloworld2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, there, sweet world!" << endl;
+    return 0;
+}
+// To run the command from Visual Studio bash terminal,
+// first compile the program using command: g++ -o a.out helloworld2.cpp
+// then run the program using: ./a.out
